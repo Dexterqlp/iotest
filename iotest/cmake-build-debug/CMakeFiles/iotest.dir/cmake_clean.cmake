a0a0d0ebd220a1cb90c9a9c94bffadd1a63caeec
Dexterqlp <qiao_peng@qq.com>
2019-05-01 09:40:16 +0800

    iotest code

---
+++ iotest/cmake-build-debug/CMakeFiles/iotest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iotest.dir/main.cpp.obj"
+  "iotest.pdb"
+  "iotest.exe"
+  "iotest.exe.manifest"
+  "libiotest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iotest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ iotest/main.cpp
@@ -0,0 +1,23 @@
+//读入数组并输出txt文件确认。
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    cout<<"please input the path:"<<endl;
+    int a[11];
+    char* Path;
+    cin>>Path;
+    ifstream in(Path);
+    for(int i=0;i<11;i++)
+        in>>a[i];
+    for(int j=0;j<=10;j++)
+        cout<<a[j]<<" ";
+    ofstream out("D:\\qlp\\testcout.txt",ios::app);
+    out<<"输出成功!"<<endl;
+    cout<<endl;
+    cout<<"success!";
+    in.close();
+    out.close();
+    return 0;
+}
